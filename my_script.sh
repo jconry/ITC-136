0c492f9743169ac433de7243fc1de5512f2dfcd8
jconry <jennifer.conry@seattlecentral.edu>
2018-10-29 20:46:42 -0700

    I added more lines of text

--- my_script.sh
+++ my_script.sh
@@ -2,4 +2,13 @@
 
 echo "hello ITC-136"
 
-echo "HI!!!"
+echo "HI!!!" 
+
+echo "This is your current working directory:"
+pwd
+
+echo "This is the version of linux you're running:"
+uname -a
+
+echo "These are your logged in users:"
+who
