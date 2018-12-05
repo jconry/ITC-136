39c27368d3c7be5f41bdec5fa2662495dd53e2eb
jconry <38932779+jconry@users.noreply.github.com>
2018-12-04 23:22:06 -0800

    Update filefinder.sh

--- filefinder.sh
+++ filefinder.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-printf "\n Please enter a file name "
+printf "\n Please enter a file name: "
 read fname
 
 #find the file
 filefound=$( readlink -f $fname )
 
+#locate the file (sudo updatedb)
+filelocated=$( locate *$fname* )
+
+#whereis the file
+filewhereis=$( whereis $fname )
+
+#executable?
+executablefile=$( ls -l $fname )
+
 #printf "\n Please enter the path to check "
 #read path2check
 
 
-echo -en "\e[94m Your File is here: $filefound"
-
-
-#if find $path2check -name $fname -print -quit |
-   #grep -q '^'; then
-  #echo "the file exists!"
-#else
-  #echo "the file does not exist!"
-#fi
+echo -en "\e[94m Your File: $executablefile
+ Found: $filefound
+ Location: $filelocated
+ Whereis: $filewhereis
+"
